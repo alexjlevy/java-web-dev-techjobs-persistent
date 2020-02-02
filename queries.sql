fc650ee67dacc01a77fe3d995638ee2c30de555a
Alex Levy <alex.j.levy@gmail.com>
2020-02-02 16:39:06 -0600

    Parts 1 +2 completed

--- queries.sql
+++ queries.sql
@@ -1,6 +1,8 @@
 ## Part 1: Test it with SQL
+Show columns from techjobs.job
 
 ## Part 2: Test it with SQL
+SELECT NAME FROM techjobs.employer WHERE location = "St. Louis";
 
 ## Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,28 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotNull(message = "Description is required!")
+    @Size(min = 10, max = 250, message = "Description must be between 10 and 250 characters")
+    private String description;
+
+    public Skill() {
+    }
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
