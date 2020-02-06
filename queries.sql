d6b10828623df3f032f098cc9fb7e0de4e04108f
Alex Levy <alex.j.levy@gmail.com>
2020-02-06 09:48:23 -0600

    SQL queries updated

--- queries.sql
+++ queries.sql
@@ -8,3 +8,8 @@ SELECT NAME FROM techjobs.employer WHERE location = "St. Louis";
 DROP TABLE techjobs.job;
 
 ## Part 4: Test it with SQL
+SELECT name, description
+FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name;
